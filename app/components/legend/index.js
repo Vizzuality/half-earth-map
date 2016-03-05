73185d7a94dd0c86030b6ea3b54b6544ca07b560
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2016-03-05 23:33:31 +0100

    Adds the structure for the legend

---
+++ app/components/legend/index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+import Vue from 'vue';
+
+import Store from '../../store';
+
+import template from './template.html';
+import './style.css';
+
+export default Vue.extend({
+
+  template,
+
+  computed: {
+    layers() {
+      return Store.state.layers;
+    },
+    activeLayers() {
+      const res = [];
+
+      for (let i = 0, j = this.layers.length; i < j; i++) {
+        const layer = this.layers[i];
+
+        if (!layer.options && layer.active ||
+          layer.options && layer.active === layer.name) {
+          res.push(layer);
+        }
+
+        if (layer.options && layer.active !== layer.name) {
+          for (let k = 0, l = layer.options.length; k < l; k++) {
+            if (layer.options[k].name === layer.active) {
+              res.push(layer.options[k]);
+              break;
+            }
+          }
+        }
+      }
+
+      return res;
+    }
+  }
+
+});
