65d9f7a97b3631deea0c7a93317d0464e2641234
David Inga <davidsingal@icloud.com>
2016-03-14 16:21:20 +0100

    Updates

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
 'use strict';
 
-import Vue from 'vue';
-
 import './app.css';
-import Map from './components/map';
-import MapControls from './components/controls';
-import Legend from './components/legend';
-import Modal from './components/modal';
-
-new Vue({
-
-  el: '#app',
-
-  components: {
-    'map': Map,
-    'map-controls': MapControls,
-    'legend': Legend,
-    'modal': Modal
-  }
-
-});
